39a485e1b0a55e027216ace27df6fcf15d00659d
tazro inutano ohta <inutano@gmail.com>
2013-09-18 16:22:09 +0900

    getting list from csv

--- app.rb
+++ app.rb
@@ -5,6 +5,7 @@ require "sinatra/activerecord"
 require "haml"
 require "sass"
 
+require "open-uri"
 require "./ballot"
 
 ENV["DATABASE_URL"] ||= "sqlite3:///database.sqlite"
@@ -16,10 +17,28 @@ class OpenScienceAward < Sinatra::Base
   set :haml, :format => :html5
   set :haml, :escape_html => true
   
+  def csv_importer(dsw)
+    csv_raw = open(app_root + "/#{dsw}.csv", "r:utf-8").readlines
+    csv_head = csv_raw.shift
+    csv_raw.map do |line_n|
+      line = line_n.split(",")
+      { name: line[0],
+        dev_by: line[1],
+        url: line[2].gsub("http://",""),
+        note: line[3] }
+    end
+  end
+  
+  def nominates
+    { database: csv_importer("database"),
+      software: csv_importer("software"),
+      web: csv_importer("web") }
+  end
+
   helpers do
     def app_root
       "#{env["rack.url_scheme"]}://#{env["HTTP_HOST"]}#{env["SCRIPT_NAME"]}"
-    end
+    end    
   end
   
   get "/:source.css" do
@@ -31,7 +50,9 @@ class OpenScienceAward < Sinatra::Base
   end
   
   post "/confirm" do
-    @vote = params["vote"]
+    @database = params[:database]
+    @software = params[:software]
+    @web = params[:web]
     haml :confirm
   end
   
